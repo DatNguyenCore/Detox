572b52a9d002ed36caccf59d15b08c886654e605
datnt <39941143+DatNguyenCore@users.noreply.github.com>
2023-04-01 15:32:13 +0700

    add go back to white list

--- app/src/main/java/com/example/detox/fragments/WhiteListFragment.java
+++ app/src/main/java/com/example/detox/fragments/WhiteListFragment.java
@@ -1,5 +1,7 @@
 package com.example.detox.fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -10,15 +12,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.detox.R;
 import com.example.detox.adapter.WhiteListAdapter;
@@ -34,6 +42,8 @@ import java.util.List;
  */
 public class WhiteListFragment extends Fragment {
     private static final String TAG = "WhiteListFragment";
+    private FragmentActivity mActivity;
+    private NavController mNavController;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -42,23 +52,11 @@ public class WhiteListFragment extends Fragment {
 
     private ArrayList<WhiteListModal> listInstalledApp = new ArrayList();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public WhiteListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WhiteListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static WhiteListFragment newInstance(String param1, String param2) {
         WhiteListFragment fragment = new WhiteListFragment();
         Bundle args = new Bundle();
@@ -71,10 +69,6 @@ public class WhiteListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         this.getAllApplications();
     }
@@ -93,16 +87,41 @@ public class WhiteListFragment extends Fragment {
 
                 indexApp++;
             }
-
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_white_list, container, false);
+        View inflatedView = inflater.inflate(R.layout.fragment_white_list, container, false);
+
+        this.setupToolBar(inflatedView);
+
+        return inflatedView;
+    }
+
+    private void setupToolBar(View inflatedView) {
+        Toolbar myToolbar = inflatedView.findViewById(R.id.toolbar_white_list);
+
+        AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
+        appCompatActivity.setSupportActionBar(myToolbar);
+        ActionBar actionBar = appCompatActivity.getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mActivity.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                mNavController.popBackStack();
+                return false;
+            }
+        });
+
     }
 
     @Override
@@ -114,27 +133,13 @@ public class WhiteListFragment extends Fragment {
         ListView mListView = view.findViewById(R.id.list_view_white_list);
         mListView.setAdapter(whiteListAdapter);
 
-        Toolbar myToolbar = view.findViewById(R.id.toolbar_white_list);
-//        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.d(TAG, "onClick: ");
-////                Navigation.findNavController(view).navigate(R.id.action_whiteListFragment_to_mainFragment);
-//            }
-//        });
-        AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
-        appCompatActivity.setSupportActionBar(myToolbar);
-
-        ActionBar actionBar = appCompatActivity.getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-
+        mNavController = Navigation.findNavController(view);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        Log.d(TAG, "onOptionsItemSelected: ");
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
 
-        return super.onOptionsItemSelected(item);
+        mActivity = requireActivity();
     }
 }
