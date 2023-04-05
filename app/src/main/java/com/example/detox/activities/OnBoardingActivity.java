b98c9cd5f2088b1ebabc7a1e289b9578d6a5dbe4
datnt <39941143+DatNguyenCore@users.noreply.github.com>
2023-04-05 19:47:01 +0700

    navigate when install

--- app/src/main/java/com/example/detox/activities/OnBoardingActivity.java
+++ app/src/main/java/com/example/detox/activities/OnBoardingActivity.java
@@ -1,16 +1,25 @@
 package com.example.detox.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
 import com.example.detox.MainActivity;
+import com.example.detox.configs.AppSharedPreferences;
 import com.example.detox.databinding.ActivityOnBoardingBinding;
 import com.example.detox.windows.ConfirmWindow;
 import com.example.detox.windows.OnConfirmWindowClickListener;
 
 public class OnBoardingActivity extends BaseCompatActivity implements OnConfirmWindowClickListener {
+    private static final String TAG = "OnBoardingActivity";
+    public static final String HONE_INTENT_DURATION_OVERLAY = "HONE_INTENT_DURATION_OVERLAY";
 
     private ActivityOnBoardingBinding binding;
+    final AppSharedPreferences appSharedPreferences = new AppSharedPreferences(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +37,29 @@ public class OnBoardingActivity extends BaseCompatActivity implements OnConfirmW
         });
     }
 
+
+    private void requestOverlayPermission() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + this.getPackageName()));
+//        mActivityResultLauncher.launch(intent);
+        startActivity(intent);
+    }
+
     @Override
     public void onConfirm(boolean allow) {
         if(allow) {
-            startActivity(MainActivity.class);
+            requestOverlayPermission();
         } else  {
             startActivity(MainActivity.class);
         }
+
+        appSharedPreferences.putBooleanValue(AppSharedPreferences.INSTALLED_APP, true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        startActivity(MainActivity.class);
     }
 }
